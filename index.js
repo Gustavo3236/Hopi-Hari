354a498ca78cd30ac4fe1324fbd1ff65aff96b67
Gustavo <90078770+Gustavo3236@users.noreply.github.com>
2023-02-13 23:48:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,23 +5,3 @@ botaoMenu.addEventListener("click", () => {
     botaoMenu = menu.classList.toggle("menu--ativo");
 })
 
-let menuListaBotao = document.querySelector(".menu-lista__botao--atracoes");
-let menuAtracoes = document.querySelector(".menu-atracoes");
-
-menuListaBotao.addEventListener("click", () => {
-    menuListaBotao = menuAtracoes.classList.toggle("menu-atracoes--ativo");
-})
-
-let menuListaEventosBotao = document.querySelector(".menu-lista__botao--eventos");
-let menuEventos = document.querySelector(".menu-eventos");
-
-menuListaEventosBotao.addEventListener("click", () => {
-    menuListaEventosBotao = menuEventos.classList.toggle("menu-eventos--ativo");
-})
-
-let menuListaMaisBotao = document.querySelector(".menu-lista__botao--mais");
-let menuMais = document.querySelector(".menu-mais");
-
-menuListaMaisBotao.addEventListener("click", () => {
-    menuListaMaisBotao = menuMais.classList.toggle("menu-mais--ativo");
-})
